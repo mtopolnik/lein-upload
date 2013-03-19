b9a3a52439dd531cb8c702966610b6faa8cd963a
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-19 15:59:41 +0100

    Move to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/lein-upload "0.1.0"
+(defproject com.ingemark/lein-upload "0.1.1-SNAPSHOT"
   :description "Upload a file to a repository"
   :url "https://github.com/Inge-mark/bundle-pbxis-ws"
   :license {:name "Eclipse Public License"
