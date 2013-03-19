cf663667a091df33c6cc049548c54df124c979c2
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-19 15:58:10 +0100

    init

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject com.ingemark/lein-upload "0.1.0"
+  :description "Upload a file to a repository"
+  :url "https://github.com/Inge-mark/bundle-pbxis-ws"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in :leiningen
+  :lein-release {:deploy-via :clojars}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [s3-wagon-private "1.1.2"]])
+
---
+++ src/leiningen/upload.clj
@@ -0,0 +1,38 @@
+(ns leiningen.upload
+  (require (leiningen [deploy :refer [repo-for]])
+           [leiningen.core.main :as main]
+           (clojure.java [shell :as sh] [io :as io])
+           [clojure.string :as s]
+           [cemerick.pomegranate.aether :as aether])
+  (import cemerick.pomegranate.aether.PomegranateWagonProvider
+          org.apache.maven.wagon.repository.Repository
+          org.apache.maven.wagon.authentication.AuthenticationInfo))
+
+(set! *warn-on-reflection* true)
+
+(defn abort [fmt & args] (main/abort (apply format fmt args)))
+
+(defn sh! [& args]
+  (apply println "$" args)
+  (let [res (apply sh/sh args)]
+    (print (:out res)) (print (:err res)) (flush)
+    (when-not (zero? (:exit res)) (abort "Command failed with exit code %s: %s" (:exit res) args))))
+
+(defn upload [project filename reponame repodir]
+  (let [repo (second (leiningen.deploy/repo-for project reponame))
+        repo-obj (Repository. "upload" (:url repo))]
+    (println "Upload" filename "==>" (:url repo))
+    (doto (.lookup (PomegranateWagonProvider.) (.getProtocol repo-obj))
+      (.connect repo-obj (doto (AuthenticationInfo.)
+                           (.setUserName (:username repo))
+                           (.setPassword (:password repo))
+                           (.setPassphrase (:passphrase repo))))
+      (.put (io/file filename) (format "%s/%s" repodir filename)))))
+
+#_(let [files {[(symbol (:group project) (:name project)) (:version project) :extension "tgz"]
+               tarfile}]
+    (aether/deploy-artifacts
+     :artifacts (keys files)
+     :files files
+     :transfer-listener :stdout
+     :repository [(leiningen.deploy/repo-for project "releases")]))
